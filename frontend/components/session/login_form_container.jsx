6ab7053f656a93113c1fd86201c881e215c46669
Kadeem Jackson <kdjackson35@gmail.com>
2020-03-04 02:43:38 -0500

    Changes to login form container

--- frontend/components/session/login_form_container.jsx
+++ frontend/components/session/login_form_container.jsx
@@ -4,10 +4,10 @@ import { login, clearErrors } from "../../actions/session_actions";
 import SessionForm from "./session_form";
 import { openModal, closeModal } from "../../actions/modal_actions";
 
-const mapStateToProps = state => {
+const mapStateToProps = ({ errors }) => {
   return {
     formType: "Log In",
-    errors: state.errors.session,
+    errors: errors.session,
     // header: <h2 className="session-header">Welcome back.</h2>,
     // footer: <p>No account?</p>,
   };
@@ -15,15 +15,15 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
   return {
-    closeModal: () => dispatch(closeModal()),
     clearErrors: () => dispatch(clearErrors()),
     processForm: user => dispatch(login(user)),
     otherForm: (
       <button
-        className="other-form-button"
-        onClick={() => dispatch(openModal("Sign Up"))}
+      className="other-form-button"
+      onClick={() => dispatch(openModal("Sign Up"))}
       >Sign Up</button>
     ),
+    closeModal: () => dispatch(closeModal()),
   };
 };
 
