d489201c94877cac1f1a9b40ec384406fe9001c7
Kadeem Jackson <kdjackson35@gmail.com>
2024-12-12 12:10:32 -0500

    Update files to fix robocop linter

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,42 +22,43 @@ class User < ApplicationRecord
   validates :username, :email, :name, :session_token, :password_digest, presence: true
   validates :username, :email, :session_token, uniqueness: true
   validates :password, length: { minimum: 6, allow_nil: true }
-  
+
   after_initialize :ensure_session_token
 
   has_many :stories,
-  primary_key: :id,
-  foreign_key: :author_id,
-  class_name: "Story",
-  dependent: :destroy
+           primary_key: :id,
+           foreign_key: :author_id,
+           class_name: 'Story',
+           dependent: :destroy
 
   has_many :responses,
-  primary_key: :id,
-  foreign_key: :author_id,
-  class_name: "Response",
-  dependent: :destroy
+           primary_key: :id,
+           foreign_key: :author_id,
+           class_name: 'Response',
+           dependent: :destroy
 
   attr_reader :password
 
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     return nil unless user
+
     user.valid_password?(password) ? user : nil
   end
 
   def self.generate_session_token
-    SecureRandom::urlsafe_base64(16)
+    SecureRandom.urlsafe_base64(16)
   end
 
   def reset_session_token!
-    self.session_token = User.generate_session_token
-    self.save!
-    self.session_token
+    session_token = User.generate_session_token
+    save!
+    session_token
   end
 
   def valid_password?(password)
-    bcrypt_password = BCrypt::Password.new(self.password_digest)
-      .is_password?(password)
+    BCrypt::Password.new(password_digest)
+                    .is_password?(password)
   end
 
   def password=(password)
@@ -66,8 +67,8 @@ class User < ApplicationRecord
   end
 
   private
+
   def ensure_session_token
     self.session_token ||= User.generate_session_token
   end
-  
 end
